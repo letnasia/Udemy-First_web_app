57d9992ebb913daad224614cc9a5bc7fc24cba47
Anastasiia Letnianko <funnynaska@gmail.com>
2023-12-12 20:43:42 +0200

    Initial commit 2

--- web.py
+++ web.py
@@ -1,13 +1,13 @@
 import functions
 import streamlit as st
 
-todos = functions.get_todos("/Users/nasya/Python/Udemy/web1/todos.txt")
+todos = functions.get_todos()
 
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
-    functions.write_todos(todos, filepath="/Users/nasya/Python/Udemy/web1/todos.txt")
+    functions.write_todos(todos)
 
 
 st.title("My Todo App")
@@ -17,7 +17,7 @@ for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        functions.write_todos(todos, filepath="/Users/nasya/Python/Udemy/web1/todos.txt")
+        functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
 st.text_input(label=" ", placeholder="Add a new todo...",
